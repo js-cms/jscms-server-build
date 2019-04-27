f6789a9e7d90c5daa52793e423c1da587b04595e
lisniuse <17560235@qq.com>
2019-04-28 00:50:19 +0800

    feat: add more.

--- bin/build.js
+++ bin/build.js
@@ -2,15 +2,21 @@
 
 const fsAction = require('../lib/fsAction');
 const chalk = require('chalk');
+const path = require('path');
+const base = path.resolve('.');
+const toolRoot = path.resolve(`${__dirname}/../`);
 
-console.log(`\n`);
+console.log('\n');
 const actions = [
-  ['remove', '/build', 'Remove a folder called `build`.'],
-  ['mkdir', '/build', 'Create a folder called `build`.'],
-  ['copy', '/app', '/build/app', 'Copy the `app` folder to the `build` directory.'],
-  ['copy', '/config', '/build/config', 'Copy the `config` folder to the `build` directory.'],
-  ['rebuildNpmConfig', '/package.json' , '/build/package.json', 'Rebuild `package.json`'],
-  ['copy', '/README.md' , '/build/README.md', 'Copy the `README.md` file to the `build` directory']
+  ['remove', `${base}/build`, 'Remove a folder called build.'],
+  ['mkdir', `${base}/build`, 'Create a folder called build.'],
+  ['mkdir', `${base}/build/ssl`, 'Create a folder called ssl.'],
+  ['copy', `${base}/app`, '/build/app', 'Copy the app folder to the build directory.'],
+  ['copy', `${base}/config`, '/build/config', 'Copy the config folder to the build directory.'],
+  ['rebuildNpmConfig', `/package.json` , '/build/package.json', 'Rebuild package.json'],
+  ['copy', `${toolRoot}/lib/vector/README.md` , '/build/README.md', 'Copy the README.md file to the build directory'],
+  ['copy', `${toolRoot}/lib/vector/jscms.conf` , '/build/jscms.conf', 'Copy the jscms.conf file to the build directory'],
+  ['copy', `${toolRoot}/lib/vector/nginx.log` , '/build/nginx.log', 'Copy the nginx.log file to the build directory']
 ];
 fsAction(actions);
 console.log(`\n${chalk.bgGreen(chalk.black((' DONE ')))} ${chalk.green('Build complete.')}\n`);
---
+++ lib/vector/README.md
@@ -0,0 +1,34 @@
+# Jscms
+
+欢迎使用Jscms，请访问以下链接获取帮助。
+
+- 官方网站：[jscms.top](http://www.jscms.top/)。
+- GitHub地址：[https://github.com/js-cms](https://github.com/js-cms)。
+
+## 简单帮助
+
+当你使用jscms-cli初始化完毕后，你应该对项目进行依赖安装：
+
+```bash
+npm install
+```
+
+安装完毕依赖之后，运行以下命令启动项目
+
+```bash
+npm start
+```
+
+停止项目的命令如下：
+
+```bash
+npm stop
+```
+
+## https配置
+
+在应用程序目录下有一个名为 ``ssl`` 的文件夹，请配合nginx配置文件来进行修改。
+
+## nginx 配置
+
+在应用程序目录下有一个名为 ``jscms.conf`` 的nginx配置参考。
