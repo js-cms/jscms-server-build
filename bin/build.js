6fca96d5b10408abf1e01dc45707c9f196b8a18a
lisniuse <17560235@qq.com>
2019-05-01 23:32:26 +0800

    feat: remove folder upload.

--- bin/build.js
+++ bin/build.js
@@ -8,11 +8,12 @@ const toolRoot = path.resolve(`${__dirname}/../`);
 
 console.log('\n');
 const actions = [
-  ['remove', `${base}/build`, 'Remove a folder called build.'],
-  ['mkdir', `${base}/build`, 'Create a folder called build.'],
-  ['mkdir', `${base}/build/ssl`, 'Create a folder called ssl.'],
-  ['copy', `${base}/app`, '/build/app', 'Copy the app folder to the build directory.'],
-  ['copy', `${base}/config`, '/build/config', 'Copy the config folder to the build directory.'],
+  ['remove', `${base}/build`, `Delete a folder ${base}/build`],
+  ['mkdir', `${base}/build`, `Create a folder ${base}/build`],
+  ['mkdir', `${base}/build/ssl`, `Create a folder ${base}/build/ssl`],
+  ['copy', `${base}/app`, '/build/app', `Copy folders ${base}/app to /build/app`],
+  ['remove', `${base}/build/app/static/upload`, `Delete a folder ${base}/build/app/static/upload`],
+  ['copy', `${base}/config`, '/build/config', `Copy folders ${base}/config to /build/config`],
   ['rebuildNpmConfig', `/package.json` , '/build/package.json', 'Rebuild package.json'],
   ['copy', `${toolRoot}/lib/vector/README.md` , '/build/README.md', 'Copy the README.md file to the build directory'],
   ['copy', `${toolRoot}/lib/vector/jscms.conf` , '/build/jscms.conf', 'Copy the jscms.conf file to the build directory'],
